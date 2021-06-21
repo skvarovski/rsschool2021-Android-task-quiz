a78dff8dd9b7c0dea84ef8b984a9d7a09c038053
SkvarovskiXenon <skvarovski@gmail.com>
2021-06-21 22:20:24 +0300

    init: result fragment

--- app/src/main/java/com/rsschool/quiz/ResultFragment.kt
+++ app/src/main/java/com/rsschool/quiz/ResultFragment.kt
@@ -6,12 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class QuizFragment : Fragment() {
+class ResultFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -19,7 +17,8 @@ class QuizFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        return inflater.inflate(R.layout.fragment_result, container, false)
     }
+
 
 }
