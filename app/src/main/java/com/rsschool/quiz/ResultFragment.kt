058d7c29e61bd99598b871bbdd54895dc0c7e544
SkvarovskiXenon <skvarovski@gmail.com>
2021-06-23 21:19:25 +0300

    init: result fragment

--- app/src/main/java/com/rsschool/quiz/ResultFragment.kt
+++ app/src/main/java/com/rsschool/quiz/ResultFragment.kt
@@ -1,5 +1,6 @@
 package com.rsschool.quiz
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,21 +13,28 @@ class ResultFragment : Fragment() {
     private val bi get() = _bi!!
     private lateinit var routerQuiz: MainActivity
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         routerQuiz = context as MainActivity
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _bi = FragmentResultBinding.inflate(layoutInflater)
-        return _bi?.root
+        _bi = FragmentResultBinding.inflate(inflater, container, false)
+        val view = bi.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
 
     }
@@ -37,7 +45,6 @@ class ResultFragment : Fragment() {
         super.onDestroy()
         _bi = null
     }
-
 
 
 }
